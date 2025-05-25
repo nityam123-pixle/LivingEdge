b68846619631cc31c5b5c9579e0a304ab3e92b14
Nityam Suchak <apple@Nityams-MacBook-Air.local>
2025-05-25 11:01:58 +0530

    Changed the Hero section text

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -27,9 +27,9 @@ const HeroSection = () => {
                     initial="offscreen"
                     whileInView="onscreen"
                     variants={desVariants}
-                    className="h1 font-serif bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent leading-snug tracking-tight text-center"
+                    className="h1 bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent leading-snug tracking-tight text-center"
                 >
-                    Follow The Next Wait For The Next <br /> - Living Edge
+                    Follow The Next Wait For The Next <br /> <span className="font-bold text-6xl bg-gradient-to-r from-[#4F3F63] via-[#F0966A] to-[#FCC797] bg-clip-text text-transparent">- Living Edge </span>
                 </motion.h1>
 
                 <motion.p
